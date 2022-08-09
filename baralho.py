92e1abbfc7f2be02c490b8c39e10779857986e9f
coot <claudio-oliveira.santos@gft.com>
2022-08-09 10:35:47 -0300

    Adicionando a classe test_baralho.py

--- baralho.py
+++ baralho.py
@@ -7,11 +7,19 @@ class Baralho():
     def __init__(self):
         self.cartas = []
         for naipe in ["Paus", "Copas", "Espadas", "Ouros"]:
-            for numero in range(1,11):
-                carta = {"naipe": naipe, "numero": str(numero), "jogador": None}
+            for numero in range(1, 11):
+                carta = {
+                    "naipe": naipe,
+                    "numero": str(numero),
+                    "jogador": None
+                }
                 self.cartas.append(carta)
             for numero in ["Dama", "Valete", "Rei"]:
-                carta = {"naipe": naipe, "numero": numero, "jogador": None}
+                carta = {
+                    "naipe": naipe,
+                    "numero": numero,
+                    "jogador": None
+                }
                 self.cartas.append(carta)
         self.embaralha()
         self.__index_generator__ = 0
@@ -34,12 +42,14 @@ class Baralho():
     def embaralha(self):
         return random.shuffle(self.cartas)
 
-
-
-
-
-
-
-
-
-
+    def somar_pontos(self, cartas):
+        soma = 0
+        for carta in cartas:
+            if isinstance(carta['numero'], str):
+                soma += 10
+            else:
+                if carta['numero'] == 1:
+                    soma += 11
+                else:
+                    soma += carta['numero']
+        return soma
---
+++ test_baralho.py
@@ -0,0 +1,51 @@
+from baralho import Baralho
+
+def test_somar_pontos_numeros():
+    cartas = [
+        {"naipe": "Ouros", "numero": 8, "jogador": None},
+        {"naipe": "Copas", "numero": 2, "jogador": None},
+        {"naipe": "Ouros", "numero": 9, "jogador": None}
+    ]
+    baralho = Baralho()
+    pontos = baralho.somar_pontos(cartas)
+    assert pontos == 19
+
+def test_somar_pontos_figuras():
+    cartas = [
+        {"naipe": "Ouros", "numero": "Rei", "jogador": None},
+        {"naipe": "Copas", "numero": "Dama", "jogador": None}
+    ]
+    baralho = Baralho()
+    pontos = baralho.somar_pontos(cartas)
+    assert pontos == 20
+
+
+def test_soma_pontos_com_as_igual_a_onze():
+    cartas = [
+        {"naipe": "Ouros", "numero": 1, "jogador": None},
+        {"naipe": "Copas", "numero": 9, "jogador": None}
+    ]
+    baralho = Baralho()
+    pontos = baralho.somar_pontos(cartas)
+    assert pontos == 20
+
+def test_soma_pontos_com_as_igual_a_um():
+    cartas = [
+        {"naipe": "Ouros", "numero": 10, "jogador": None},
+        {"naipe": "Ouros", "numero": "Rei", "jogador": None},
+        {"naipe": "Copas", "numero": 1, "jogador": None}
+    ]
+    baralho = Baralho()
+    pontos = baralho.somar_pontos(cartas)
+    print(pontos)
+
+
+test_somar_pontos_numeros()
+
+test_somar_pontos_figuras()
+
+test_soma_pontos_com_as_igual_a_onze()
+
+test_soma_pontos_com_as_igual_a_um()
+
+
