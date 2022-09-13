cccefd1e1e07927dfb016ba3f4a3e86bfbf1d6d8
coot <claudio-oliveira.santos@gft.com>
2022-09-13 11:09:22 -0300

    Adicionando mackando o metodo que mostra as cartas

--- test_jogo.py
+++ test_jogo.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, MagicMock
 
 from baralho import Baralho
 from dealer import Dealer
@@ -28,7 +29,7 @@ class TestJogo(unittest.TestCase):
 
     def test_numero_de_jogadores_igual_a_3(self):
         numero_de_jogadores = 3
-        esperado = numero_de_jogadores + 1
+        esperado = numero_de_jogadores + 1 # numero de jogadores + dealer
         self.jogo.definir_jogadores()
         resultado = len(self.jogo.jogadores)
         self.assertEqual(esperado, resultado)
@@ -45,6 +46,33 @@ class TestJogo(unittest.TestCase):
         self.jogador.values = self.baralho.somar_pontos(self.jogador.cards)
         resultado = self.jogador.values
         self.assertEqual(resultado, esperado)
+
+
+    @patch("jogo.Jogo.mostrar_as_cartas")
+    def test_mostrar_as_cartas(self, mostrar_as_cartas_mock: MagicMock):
+        jogador = Player()
+        dealer = Dealer()
+        baralho = Baralho()
+        baralho.dar_as_cartas(dealer)
+        baralho.dar_as_cartas(jogador)
+        jogadores = [dealer, jogador]
+        jogo = Jogo()
+        jogo.jogadores = jogadores
+        mostrar_as_cartas_mock.side_effect = "Jogador: Dealer" \
+                                             "Cartas:" \
+                                             "{'naipe': 'Paus', 'numero': '8', 'jogador': 'Dealer', " \
+                                             "'id': '0bb64b4b-299c-444c-9ee5-a1f82d9f782f'}" \
+                                             "{'naipe': 'Copas', 'numero': '3', 'jogador': 'Dealer', " \
+                                             "'id': 'a343e54f-3f18-44d3-b1b7-ed9bf25f9b40'}" \
+                                             "Jogador: Maria Morgan" \
+                                             "Cartas:" \
+                                             "{'naipe': 'Ouros', 'numero': 'Valete', 'jogador': 'Maria Morgan', " \
+                                             "'id': '68ddec54-af10-40e7-8b51-306d20cd79ef'}" \
+                                             "{'naipe': 'Copas', 'numero': 'Valete', 'jogador': 'Maria Morgan', " \
+                                             "'id': '402b6e8c-7b63-4c2c-9c79-75e628b6b04b'}"
+        resposta = self.jogo.mostrar_as_cartas()
+        print(resposta)
+
 
 
 
